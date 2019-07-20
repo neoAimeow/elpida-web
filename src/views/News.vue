edc76cb2bda15d5109f0908971d99b317770c066
aimeow <a18814888787@gmail.com>
2019-07-20 15:14:32 +0800

    uypdate

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,39 +1,50 @@
 <template>
     <div class="news-container">
+
         <div id="news-title" :class="barFixed == true ? 'isFixed' :''">
-            <span style="font-size: 40px;">新闻</span>
-            <el-divider direction="vertical"></el-divider>
-            <span style="font-family: HYZhengYuan-CEW">{{date}}</span>
+            <div class="news-title-inner">
+                <div class="news-title-left">
+                    <span style="font-size: 40px;">新闻</span>
+                    <el-divider direction="vertical"></el-divider>
+                    <span style="font-family: HYZhengYuan-CEW">{{date}}</span>
+                </div>
+
+                <div class="news-title-right">
+                    <el-date-picker @change="dateChange" v-model="date" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+                </div>
+            </div>
+
             <el-divider></el-divider>
     </div>
 
-        <div class="news" v-loading="loading">
-            <div class="news-icon">
-                <i @click="goYesterday" class="el-icon-back"></i>
-
-            </div>
-
-            <div id="news-content">
-                <div v-if="dayType === 1" style="text-align: center;margin-top: 220px;">当天没有新闻</div>
-                <div v-else v-for="(newsCase , key) in news" :key="key">
-                    <!--                <span style="font-family: iekiexingkongzhiyi">{{newsCase.content == '' ? newsCase.title: newsCase.content}}</span>-->
-                    <span>{{newsCase.content == '' ? newsCase.title: newsCase.content}}</span>
-
-                    <el-divider content-position="right">
-                        <div>
-                            <i class="el-icon-potato-strips"></i>
-                            <el-divider direction="vertical"></el-divider>
-                            <span style="font-family: HYZhengYuan-CEW">{{newsCase.src}}</span>
-                            <el-divider direction="vertical"></el-divider>
-                            <span style="font-family: HYZhengYuan-CEW">{{newsCase.dateTime| dateFormat("YYYY-MM-DD HH:mm:ss")}}</span>
-                        </div>
-                    </el-divider>
+        <div id="news-body">
+            <div class="news" v-loading="loading">
+                <div class="news-icon">
+                    <i @click="goYesterday" class="el-icon-back"></i>
                 </div>
-            </div>
-            <div style="text-align: right;" class="news-icon">
-                <i @click="goTomorrow" v-if="dayType === -1" class="el-icon-right"></i>
-            </div>
 
+                <div id="news-content">
+                    <div v-if="dayType === 1" style="text-align: center;margin-top: 220px;">当天没有新闻</div>
+                    <div v-else v-for="(newsCase , key) in news" :key="key">
+                        <!--                <span style="font-family: iekiexingkongzhiyi">{{newsCase.content == '' ? newsCase.title: newsCase.content}}</span>-->
+                        <span>{{newsCase.content == '' ? newsCase.title: newsCase.content}}</span>
+
+                        <el-divider content-position="right">
+                            <div>
+                                <i class="el-icon-potato-strips"></i>
+                                <el-divider direction="vertical"></el-divider>
+                                <span style="font-family: HYZhengYuan-CEW">{{newsCase.src}}</span>
+                                <el-divider direction="vertical"></el-divider>
+                                <span style="font-family: HYZhengYuan-CEW">{{newsCase.dateTime| dateFormat("YYYY-MM-DD HH:mm:ss")}}</span>
+                            </div>
+                        </el-divider>
+                    </div>
+                </div>
+                <div style="text-align: right;" class="news-icon">
+                    <i @click="goTomorrow" v-if="dayType === -1" class="el-icon-right"></i>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -50,7 +61,8 @@
                 // 0为同一天，1表示将来，-1表示过去
                 dayType: 0,
                 yesterdayDateStr: '',
-                tomorrowDateStr: ''
+                tomorrowDateStr: '',
+                dialogVisible: false
             }
         },
         created: function () {
@@ -101,8 +113,11 @@
             },
             goTomorrow() {
                 this.$router.push({query: {date: this.tomorrowDateStr}});
+            },
+            dateChange() {
+                let dateStr = this.$moment(this.date, 'YYYY-MM-DD').format('YYYYMMDD');
+                this.$router.push({query: {date: dateStr}});
             }
-
         }
     }
 </script>
@@ -114,15 +129,28 @@
         justify-content: center;
         /*align-items: center;*/
     }
+
     #news-title {
         background-color: white;
         height: 90px;
         width: 100%;
     }
+
+    .news-title-inner {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between	;
+        align-items: flex-end;
+    }
+
     .news-icon {
         width: 5%;
         font-size: 30px;
     }
+    #news-body {
+        margin-top: 15px;
+    }
+
     #news-content {
         /*background-color: red;*/
         width: 90%;
